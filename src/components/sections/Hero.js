29786f1da70c3a5ec55dde5c9c0c7f229c980d35
broken99records <40528283+broken99records@users.noreply.github.com>
2022-12-13 04:45:00 -0500

    done
    changing title and button text

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,11 +61,11 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+               <span className="text-color-primary">Garri-Hub</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                Our landing page shows commodity prices for Garri.
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
@@ -73,7 +73,7 @@ const Hero = ({
                     Get started
                     </Button>
                   <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
-                    View on Github
+                    View Prices
                     </Button>
                 </ButtonGroup>
               </div>
