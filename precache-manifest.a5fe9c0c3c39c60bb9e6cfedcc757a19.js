1dda32075a09ff35599441af96a5ca34ef651b19
broken99records <40528283+broken99records@users.noreply.github.com>
2023-02-17 08:04:53 -0500

    Updates

---
+++ precache-manifest.a5fe9c0c3c39c60bb9e6cfedcc757a19.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849c65adca42c054a921fcaba5f30aa7",
+    "url": "/Garri-hub-react/index.html"
+  },
+  {
+    "revision": "dac7ce4be43ee365f66b",
+    "url": "/Garri-hub-react/static/css/main.839dcb36.chunk.css"
+  },
+  {
+    "revision": "b1cc2f85861c04fdb15e",
+    "url": "/Garri-hub-react/static/js/2.e5947536.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/Garri-hub-react/static/js/2.e5947536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac7ce4be43ee365f66b",
+    "url": "/Garri-hub-react/static/js/main.f5e8018c.chunk.js"
+  },
+  {
+    "revision": "dc5dd54af98fefadff29",
+    "url": "/Garri-hub-react/static/js/runtime-main.7c4808cb.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/Garri-hub-react/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "bb1e3029ba49ee91622899c397a06a49",
+    "url": "/Garri-hub-react/static/media/dsBuffer.bmp.bb1e3029.png"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/Garri-hub-react/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/Garri-hub-react/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/Garri-hub-react/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/Garri-hub-react/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/Garri-hub-react/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/Garri-hub-react/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/Garri-hub-react/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/Garri-hub-react/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/Garri-hub-react/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/Garri-hub-react/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/Garri-hub-react/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "7f7d1bc5597887a0eabbc415f9ad8647",
+    "url": "/Garri-hub-react/static/media/part-hero.7f7d1bc5.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Garri-hub-react/precache-manifest.a5fe9c0c3c39c60bb9e6cfedcc757a19.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Garri-hub-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
