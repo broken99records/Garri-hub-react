55adbfbee3d0d380e0d1a0e8f3cceccccfd51768
broken99records <40528283+broken99records@users.noreply.github.com>
2023-03-14 08:13:54 -0400

    populated search route with text fields imported hashrouter for gh pages placed loading progress bar in root div replaced when rendered

--- src/views/Search.js
+++ src/views/Search.js
@@ -1,16 +1,70 @@
 import React, { Component } from 'react';
 import GenericSection from '../components/sections/GenericSection';
-import MissedVideoCallIcon from '@mui/icons-material/MissedVideoCall';
 import SearchIcon from '@mui/icons-material/Search';
+import { Card, TextField } from '@mui/material';
+//import { color } from '@mui/system';
+//import InfiniteScroll from 'react-infinite-scroll-component';
+import Box from '@mui/material/Box';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
 
 class Search extends Component {
 
 render (){
     return(
-     <GenericSection>   
-    <MissedVideoCallIcon/>
-    <SearchIcon/>
+    <GenericSection> 
+    <TextField  size='small' color='success'> </TextField> 
+
+      <Input
+          id="input-with-icon-adornment"
+          startAdornment={
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          }
+        />
+
+
+
+    <Box sx={{ '& > :not(style)': { m: 1 } }}>
+      <FormControl variant="standard">
+        <InputLabel htmlFor="input-with-icon-adornment">
+          With a start adornment
+        </InputLabel>
+        <Input
+          id="input-with-icon-adornment"
+          startAdornment={
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+      <TextField
+        id="input-with-icon-textfield"
+        label="TextField"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }}
+        variant="standard"
+      />
+      <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+        <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+        <TextField id="input-with-sx" label="With sx" variant="standard" />
+      </Box>
+    </Box>
+
+
+    <SearchIcon/>   
+    <Card/>
     </GenericSection>
        
     )
