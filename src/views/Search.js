a50efbc822d12a0591ec9d84d3622a24c6cd7895
broken99records <40528283+broken99records@users.noreply.github.com>
2023-03-16 10:16:00 -0400

    added a search input field to search  page

--- src/views/Search.js
+++ src/views/Search.js
@@ -1,74 +1,43 @@
 import React, { Component } from 'react';
 import GenericSection from '../components/sections/GenericSection';
 import SearchIcon from '@mui/icons-material/Search';
-import { Card, TextField } from '@mui/material';
+import { Card } from '@mui/material';
 //import { color } from '@mui/system';
 //import InfiniteScroll from 'react-infinite-scroll-component';
-import Box from '@mui/material/Box';
 import Input from '@mui/material/Input';
 import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
 import FormControl from '@mui/material/FormControl';
-import AccountCircle from '@mui/icons-material/AccountCircle';
+//import { color } from '@mui/system';
 
 
 class Search extends Component {
 
 render (){
     return(
-    <GenericSection> 
-    <TextField  size='small' color='success'> </TextField> 
+    <GenericSection>
 
-      <Input
-          id="input-with-icon-adornment"
-          startAdornment={
-            <InputAdornment position="start">
-              <AccountCircle />
-            </InputAdornment>
-          }
-        />
+            <FormControl variant="standard">
 
+            <InputLabel htmlFor="input-with-icon-adornment" sx={{ color: "white" }}>
+              Search
+            </InputLabel>
+            
+            <Input sx={{ color: "white" }}
+          startAdornment={<InputAdornment position="start">
+            <SearchIcon sx={{ color: "white" }} />
+          </InputAdornment>} />
 
-
-    <Box sx={{ '& > :not(style)': { m: 1 } }}>
-      <FormControl variant="standard">
-        <InputLabel htmlFor="input-with-icon-adornment">
-          With a start adornment
-        </InputLabel>
-        <Input
-          id="input-with-icon-adornment"
-          startAdornment={
-            <InputAdornment position="start">
-              <AccountCircle />
-            </InputAdornment>
-          }
-        />
-      </FormControl>
-      <TextField
-        id="input-with-icon-textfield"
-        label="TextField"
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <AccountCircle />
-            </InputAdornment>
-          ),
-        }}
-        variant="standard"
-      />
-      <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-        <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-        <TextField id="input-with-sx" label="With sx" variant="standard" />
-      </Box>
-    </Box>
+          </FormControl>       
 
 
-    <SearchIcon/>   
-    <Card/>
-    </GenericSection>
-       
+        
+        <Card />
+      </GenericSection>
+      
+      
     )
-}
+  }
 }
 
 export default Search;
